d8518c50d820e6fe1495ddf1447232e5f5900758
TaeUkChu <hippo2012@naver.com>
2022-12-26 17:34:46 +0900

    Day3 - Test Repository

---
+++ sbb/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,20 @@
+//package com.example.demo;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Getter
+//@Setter
+//@Controller
+//public class HelloController {
+//    @RequestMapping("/hello")
+//    @ResponseBody
+//
+//    public String hello() {
+//        return "Hello World";
+//    }
+//}
